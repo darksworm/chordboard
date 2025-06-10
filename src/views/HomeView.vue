7d65deb4dd1192d2b11fbc46230ffc0ca9638fdc
Ilmārs <9987548+darksworm@users.noreply.github.com>
2025-06-10 17:51:40 +0200

    feat: link to github

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import ChordBoard from '../components/ChordBoard.vue'
+import GithubCorner from "@/components/GithubCorner.vue";
 </script>
 
 <template>
   <main>
+    <GithubCorner />
     <ChordBoard />
   </main>
 </template>
